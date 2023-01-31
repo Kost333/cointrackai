7c6607bb0a2f7f5bf2f91279d3f66406d9b27fd8
Levon <levonkostanyan2021@gmail.com>
2023-02-01 03:09:50 +0400

    implement-table-and-comming-soon-components

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ var ctx3 = document.getElementById('chart3').getContext('2d');
 var ctx4 = document.getElementById('chart4').getContext('2d');
 var ctx5 = document.getElementById('chart5').getContext('2d');
 var ctx6 = document.getElementById('chart6').getContext('2d');
+var ctx7 = document.getElementById('chart7').getContext('2d');
+var ctx8 = document.getElementById('chart8').getContext('2d');
+var ctx9 = document.getElementById('chart9').getContext('2d');
 var gradient1 = ctx1.createLinearGradient(0, 0, 0, 400)
 var gradient2 = ctx2.createLinearGradient(0, 0, 0, 400)
 var gradient3 = ctx3.createLinearGradient(0, 0, 0, 400)
@@ -240,6 +243,132 @@ new Chart(ctx6, {
             data: [0, 50, 70, 50, 75, 65, 70, 60, 80, 75, 100, 90, 100],
             borderColor: "#61E224",
             backgroundColor: gradient6,
+            pointRadius: 0,
+        },]
+    },
+    options: {
+        title: {
+            display: true,
+        },
+        legend: {
+            display: false,
+        },
+        elements: {
+            line:{
+                tension: 0
+            }
+        },
+        tooltips: {
+            callbacks: {
+                label: false
+            }
+        },
+        scales: {
+            xAxes: [{
+                ticks: {
+                    display: false
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    display: false
+                }
+            }]
+        }
+    }
+});
+new Chart(ctx7, {
+    type: 'line',
+    data: {
+        labels: ["01-01", "02-01", "03-01", "04-01", "05-01", "06-01", "07-01", "08-01", "09-01"],
+        datasets: [{
+            data: [0, 50, 0, 50, 75, 65, 70, 60, 80, 75, 100, 90, 100],
+            borderColor: "#61E224",
+            backgroundColor: 'transparent',
+            pointRadius: 0,
+        },]
+    },
+    options: {
+        title: {
+            display: true,
+        },
+        legend: {
+            display: false,
+        },
+        elements: {
+            line:{
+                tension: 0
+            }
+        },
+        tooltips: {
+            callbacks: {
+                label: false
+            }
+        },
+        scales: {
+            xAxes: [{
+                ticks: {
+                    display: false
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    display: false
+                }
+            }]
+        }
+    }
+});
+new Chart(ctx8, {
+    type: 'line',
+    data: {
+        labels: ["01-01", "02-01", "03-01", "04-01", "05-01", "06-01", "07-01", "08-01", "09-01"],
+        datasets: [{
+            data: [0, 50, 0, 50, 75, 65, 70, 60, 80, 75, 100, 90, 100],
+            borderColor: "#61E224",
+            backgroundColor: 'transparent',
+            pointRadius: 0,
+        },]
+    },
+    options: {
+        title: {
+            display: true,
+        },
+        legend: {
+            display: false,
+        },
+        elements: {
+            line:{
+                tension: 0
+            }
+        },
+        tooltips: {
+            callbacks: {
+                label: false
+            }
+        },
+        scales: {
+            xAxes: [{
+                ticks: {
+                    display: false
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    display: false
+                }
+            }]
+        }
+    }
+});
+new Chart(ctx9, {
+    type: 'line',
+    data: {
+        labels: ["01-01", "02-01", "03-01", "04-01", "05-01", "06-01", "07-01", "08-01", "09-01"],
+        datasets: [{
+            data: [0, 50, 0, 50, 75, 65, 70, 60, 80, 75, 100, 90, 100],
+            borderColor: "#61E224",
+            backgroundColor: 'transparent',
             pointRadius: 0,
         },]
     },
